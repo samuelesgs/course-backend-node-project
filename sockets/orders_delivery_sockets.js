75ea53111ba0b73268c7195869789de9dec95621
samuelesgs <samuelesgs04@gmail.com>
2023-06-06 11:19:28 -0700

    Update orders_delivery_sockets.js

--- sockets/orders_delivery_sockets.js
+++ sockets/orders_delivery_sockets.js
@@ -1,12 +1,12 @@
 module.exports = (io) => {
-    const namespace = io.of('/orders/delivery')
+    const namespace = io.of('/orders/delivery');
 
     namespace.on('connection', function(socket) {
         console.log('Usuario conectado a socket /orders/delivery');
         socket.on('position', function(data) {
             console.log('Se emitio', JSON.parse(data));
             const dataParse = JSON.parse(data); // DEBE ENVIARLA EL CLIENTE(ID, LAT, LNG)
-            namespace.emit(`position/${dataParse.id_order}`, {id_order : dataParse.id_order, lat: dataParse.lat, lng : dataParse.lng}) // data emitida a kotlin
+            namespace.emit(`position/${dataParse.id_order}`, {id_order : dataParse.id_order, lat: dataParse.lat, lng : dataParse.lng}); // data emitida a kotlin
         });
         socket.on('disconnect', function(data) {
             console.log('Usuario desconectado de socket');
